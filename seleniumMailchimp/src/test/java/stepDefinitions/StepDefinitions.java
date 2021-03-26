fe8e0bdb4bb5f1324a35701edc57eabc4219825e
Anna <anna@bonnit.se>
2021-03-26 11:15:39 +0100

    First test done and working.

--- seleniumMailchimp/src/test/java/stepDefinitions/StepDefinitions.java
+++ seleniumMailchimp/src/test/java/stepDefinitions/StepDefinitions.java
@@ -5,7 +5,10 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -21,26 +24,22 @@ public class StepDefinitions {
 		
 
 			@Given("I have entered my {string}")
-			public void i_have_entered_my(String string) {
+			public void i_have_entered_my(String email) {
 				driver.get("https://login.mailchimp.com/signup/"); // Gå till webbsidan	
 				driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-				//click(driver, By.id("onetrust-accept-btn-handler"));
-				WebElement emailInput = driver.findElement(By.id("email"));
-				 emailInput.sendKeys("anna@bonnit.se"); //Skriver in e-post i sökfältet
+				sendKeys(driver, By.id("email"), (email));
 			}
 
 		
 		@Given("I have also entered {string}")
-		public void i_have_also_entered(String string)  {
-			 WebElement usernameInput = driver.findElement(By.id("new_username"));
-			 usernameInput.sendKeys("mayswallow"); //Skriver in användarnamn i sökfältet
-			 
+		public void i_have_also_entered(String username)  {
+		sendKeys(driver, By.id("new_username"), (username));
 		}
 		
-		@Given("Choose {string}")
-			public void choose(String string)  {
-			 WebElement passwordInput = driver.findElement(By.id("new_password"));
-			 passwordInput.sendKeys("NYTTlosenord!2"); //Skriver in lösenord
+		@And("I choose a {string}")
+			public void i_choose_a(String password)  {
+			sendKeys(driver, By.id("new_password"), (password));
+			click(driver, By.id("onetrust-accept-btn-handler"));
 			}
 		
 		
@@ -54,8 +53,13 @@ public class StepDefinitions {
 			//driver.quit();
 		}
 
-		/*private void click(WebDriver driver, By by) {
+		private void click(WebDriver driver, By by) {
 			(new WebDriverWait(driver, 10)).until(ExpectedConditions.elementToBeClickable(by));
 			driver.findElement(by).click();
-		}*/
+		}
+		
+		private void sendKeys(WebDriver driver, By by, String keys) {
+			new WebDriverWait(driver, 5).until(ExpectedConditions.presenceOfElementLocated(by));
+			driver.findElement(by).sendKeys(keys);
+			}
 }
--- seleniumMailchimp/target/test-classes/Features/mailchimp.feature
+++ seleniumMailchimp/target/test-classes/Features/mailchimp.feature
@@ -1,17 +1,21 @@
 Feature: Signup to Mailchimp
+Signup for an mailchimp account.
 
-Scenario: Signup to mailchimp
+Scenario Outline: Signup to mailchimp
 
-Before I have used 'chrome' as browser
+Given I have used <browser> as browser
 
-Given I have used 'chrome' as browser
+Given I have entered my <email> 
 
-Given I have entered my 'email' 
+Given I have also entered <username>
 
-Given I have also entered 'username'
-
-Given Choose 'password'
+And I choose a <password>
 
 When I press signup
 
 Then i should be signed in
+
+Examples:
+|browser|email|username|password|
+|'chrome'|'anna@bonnit.se'|'mayswallow'|'NYTTlosenord!1'|
+|'firefox'|'klingon@gmail.com'|'R2D2'|'NYTTlosenord!2'|
